5aa2a45fedf75d523482b0e64c2fa55687879495
awagne30 <anwagner@bsc.edu>
2022-03-08 13:28:03 -0600

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 
 public class Driver {
 
-	public static void printBlanks(int length){
-	String[] WordToPrint = new String[](length);
-		for (int i = 0; i < length; i++);
-			WordToPrint[i] = "_";
-		system.out.println(WordToPrint);
-	}
-
 	public static void main(String[] args) {
 		String fileName = "EvilHangman/dictionary.txt";
 
@@ -26,18 +19,18 @@ public class Driver {
 			System.out.println(word);
 		}
 		
-		printBlanks(length);
+		EvilHangman game = new EvilHangman(length);
+		while(!game.checkWin()) {
+			game.printWord();
+		}
 		/*
 		//TODO:
+		 * Start with regular hangman
 		 * prompt the user for the number of guesses - Daniel
-		 * display the number of blanks to match the letters - AJ
-		 * prompt the user to input a letter - Jordan Johnson
-		 * check word for letter - Phillip
-		 * update the word display with the letters they have guessed - Ross
-		 * maintain and possibly display list of previously guessed letters - Jordan Jones
-		 * if all blanks have been filled - user wins - Jack
-		 * counter to keep up with incorrect guesses and if exceeds number of guesses from beginning, the player loses - Daniel
-		 * loop to play again - Jordan Johnson
+		 * update the word display with the letters they have guessed
+		 * maintain and possibly display list of previously guessed letters
+		 * counter to keep up with incorrect guesses and if exceeds number of guesses from beginning, the player loses
+		 * loop to play again
 		 * 
 		 * 
 		 * Big Task: algorithm to change word based on user guesses to include a method for creating word families
