c69d42dd846b5c9788c5093490290dc5bd29a6ac
awagne30 <anwagner@bsc.edu>
2022-03-08 13:37:30 -0600

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -14,14 +14,21 @@ public class Driver {
 		System.out.print("Enter the word length: ");
 		int length = input.nextInt();
 		
+		System.out.print("Enter a number of guesses : ") ;
+		int numofguesses = reader.nextInt();
+		
 		ArrayList<String> sample = processor.getWordList(length);
 		for(String word : sample) {
 			System.out.println(word);
 		}
+		int incorrectGuesses = 0;
 		
 		EvilHangman game = new EvilHangman(length);
-		while(!game.checkWin()) {
+		while(!game.checkWin() && incorrectGuesses < numofguesses) {
 			game.printWord();
+			//Phillip's check word
+			incorrectGuesses++;
+			
 		}
 		/*
 		//TODO:
@@ -35,25 +42,8 @@ public class Driver {
 		 * 
 		 * Big Task: algorithm to change word based on user guesses to include a method for creating word families
 		*/
-	
-		public int getnumofguesses() {
-			Scanner reader = new Scanner(System.in);
-			System.out.print("Enter a number of guesses : ") ;
-			int numofguesses = reader.nextInt();
-			return numofguesses;
-		}
 
-			//counter to keep up with incorrect guesses and if exceeds number of guesses from beginning, the player loses
-		public Object incorectguesscounter(int numofguesses, object guess ) {
-			int i =0;
-			while (i < numofguesses) {
-				Object guess;
-				if ((boolean) (guess = false)) { 
-					i ++;
-				}
-			}
-			return null;
-		}
+
 }
 	
 	}
