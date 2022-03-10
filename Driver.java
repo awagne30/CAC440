9d9268ffb27869e6fbb0fabd35c5eede5bb0febf
Ross Richesin <richesinrr0@gmail.com>
2022-03-10 13:26:20 -0600

    place letter func

--- Driver.java
+++ Driver.java
@@ -18,12 +18,37 @@ public class Driver {
 		for(String word : sample) {
 			System.out.println(word);
 		}
+	}
+	//update the word display with the letters they have guessed - Ross
+	//args: user inputted letters
+	public static displayGuessedLetters(String letter) {
+		
+		List<String> guessedLetters = new ArrayList<>(Arrays.asList(strArray));
+       
+        guessedLetters.add(letter);
+		System.out.println("Letters that have been guessed: ");
+          for(String val : guessedLetters){
+          System.out.print(val + " ");
+		}
+		//return array guessedLetters to be used by other members?
+	}
 
-		public static void main(String[] args) {
-			
 
-
-
+	public static placeLetter(String[] largestGroup, String guessedLetter) {
+		List<String> placeLetter = new ArrayList<String>();
+		for(String word : largestgroup){
+			if(word.contains(guessedLetter)) {
+				for(String letter : word){
+					String onlyGuessedLetters = null;
+					if(letter == guessedLetter){
+						onlyGuessedLetters = letter;
+					}
+					onlyGuessedLetters = '_';
+				placeLetter.add(onlyGuessedLetters)
+				}
+			}
+		}
+	}
 		/*
 		//TODO:
 		 * Start with regular hangman
@@ -31,7 +56,7 @@ public class Driver {
 		 * display the number of blanks to match the letters - AJ
 		 * prompt the user to input a letter - Jordan Johnson
 		 * check word for letter - Phillip
-		 * update the word display with the letters they have guessed - Ross
+		 * update the word display with the letters they have guessed if they match - Ross
 		 * maintain and possibly display list of previously guessed letters - Jordan Jones
 		 * if all blanks have been filled - user wins - Jack
 		 * counter to keep up with incorrect guesses and if exceeds number of guesses from beginning, the player loses - Daniel
@@ -39,6 +64,6 @@ public class Driver {
 		 * 
 		 * Big Task: algorithm to change word based on user guesses to include a method for creating word families
 		*/
-	}
+	
 
 }
